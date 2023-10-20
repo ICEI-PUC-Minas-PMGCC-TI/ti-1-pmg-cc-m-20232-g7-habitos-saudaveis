04734b82de8c249d0c34ab783e0e1f197b2cd9e7
Rafaela Oliveira Ribeiro <rafaela.o.ribeiro@hotmail.com>
2023-10-20 15:16:21 -0300

    allowing the response to be seen in the console

--- codigo/metas/metas.js
+++ codigo/metas/metas.js
@@ -14,6 +14,18 @@ const validateMessage = () => {
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     validateMessage();
+
+    const formData = new FormData(form);
+    const data = Object.fromEntries(formData);
+
+    fetch('https://reqres.in/api/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }).then(res => res.json()).then(data => console.log(data));
+
 });
 
 message.addEventListener("message", () => {
