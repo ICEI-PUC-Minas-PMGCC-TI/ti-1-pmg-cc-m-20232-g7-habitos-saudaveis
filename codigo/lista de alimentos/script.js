ed8083681c5b8b2a3e385037f264f1977fe3e2de
Rafaela Oliveira Ribeiro <rafaela.o.ribeiro@hotmail.com>
2023-11-29 21:44:34 -0300

    fixing bugs

---
+++ codigo/lista de alimentos/script.js
@@ -0,0 +1,453 @@
+
+var al = {
+  "alimentos": [
+    {
+      "id": 1,
+      "nome": "Banana",
+      "categoria": "Frutas",
+      "CaloriaPor100g": 89
+    },
+    {
+      "id": 2,
+      "nome": "Arroz",
+      "categoria": "Grãos",
+      "CaloriaPor100g": 130
+    },
+    {
+      "id": 3,
+      "nome": "Salmão",
+      "categoria": "Peixes",
+      "CaloriaPor100g": 206
+    },
+    {
+      "id": 4,
+      "nome": "Espinafre",
+      "categoria": "Vegetais",
+      "CaloriaPor100g": 23
+    },
+    {
+      "id": 5,
+      "nome": "Queijo Cheddar",
+      "categoria": "Laticínios",
+      "CaloriaPor100g": 403
+    },
+    {
+      "id": 6,
+      "nome": "Maçã",
+      "categoria": "Frutas",
+      "CaloriaPor100g": 52
+    },
+    {
+      "id": 7,
+      "nome": "Macarrão",
+      "categoria": "Grãos",
+      "CaloriaPor100g": 158
+    },
+    {
+      "id": 8,
+      "nome": "Atum",
+      "categoria": "Peixes",
+      "CaloriaPor100g": 144
+    },
+    {
+      "id": 9,
+      "nome": "Brócolis",
+      "categoria": "Vegetais",
+      "CaloriaPor100g": 35
+    },
+    {
+      "id": 10,
+      "nome": "Leite",
+      "categoria": "Laticínios",
+      "CaloriaPor100g": 61
+    },
+    {
+      "id": 11,
+      "nome": "Laranja",
+      "categoria": "Frutas",
+      "CaloriaPor100g": 43
+    },
+    {
+      "id": 12,
+      "nome": "Quinoa",
+      "categoria": "Grãos",
+      "CaloriaPor100g": 120
+    },
+    {
+      "id": 13,
+      "nome": "Sardinha",
+      "categoria": "Peixes",
+      "CaloriaPor100g": 208
+    },
+    {
+      "id": 14,
+      "nome": "Cenoura",
+      "categoria": "Vegetais",
+      "CaloriaPor100g": 41
+    },
+    {
+      "id": 15,
+      "nome": "Iogurte",
+      "categoria": "Laticínios",
+      "CaloriaPor100g": 61
+    },
+    {
+      "id": 16,
+      "nome": "Pêra",
+      "categoria": "Frutas",
+      "CaloriaPor100g": 57
+    },
+    {
+      "id": 17,
+      "nome": "Feijão",
+      "categoria": "Grãos",
+      "CaloriaPor100g": 128
+    },
+    {
+      "id": 18,
+      "nome": "Truta",
+      "categoria": "Peixes",
+      "CaloriaPor100g": 146
+    },
+    {
+      "id": 19,
+      "nome": "Couve",
+      "categoria": "Vegetais",
+      "CaloriaPor100g": 49
+    },
+    {
+      "id": 20,
+      "nome": "Manteiga",
+      "categoria": "Laticínios",
+      "CaloriaPor100g": 717
+    },
+    {
+      "id": 21,
+      "nome": "Morango",
+      "categoria": "Frutas",
+      "CaloriaPor100g": 32
+    },
+    {
+      "id": 22,
+      "nome": "Milho",
+      "categoria": "Grãos",
+      "CaloriaPor100g": 86
+    },
+    {
+      "id": 23,
+      "nome": "Bacalhau",
+      "categoria": "Peixes",
+      "CaloriaPor100g": 105
+    },
+    {
+      "id": 24,
+      "nome": "Abóbora",
+      "categoria": "Vegetais",
+      "CaloriaPor100g": 26
+    },
+    {
+      "id": 25,
+      "nome": "Leite de Cabra",
+      "categoria": "Laticínios",
+      "CaloriaPor100g": 68
+    },
+    {
+      "id": 26,
+      "nome": "Uva",
+      "categoria": "Frutas",
+      "CaloriaPor100g": 69
+    },
+    {
+      "id": 27,
+      "nome": "Cevada",
+      "categoria": "Grãos",
+      "CaloriaPor100g": 354
+    },
+    {
+      "id": 28,
+      "nome": "Pargo",
+      "categoria": "Peixes",
+      "CaloriaPor100g": 125
+    },
+    {
+      "id": 29,
+      "nome": "Ervilhas",
+      "categoria": "Vegetais",
+      "CaloriaPor100g": 81
+    },
+    {
+      "id": 30,
+      "nome": "Leite de Soja",
+      "categoria": "Laticínios",
+      "CaloriaPor100g": 54
+    },
+    {
+      "id": 31,
+      "nome": "Abacaxi",
+      "categoria": "Frutas",
+      "CaloriaPor100g": 50
+    },
+    {
+      "id": 32,
+      "nome": "Couscous",
+      "categoria": "Grãos",
+      "CaloriaPor100g": 112
+    },
+    {
+      "id": 33,
+      "nome": "Bacalhau",
+      "categoria": "Peixes",
+      "CaloriaPor100g": 82
+    },
+    {
+      "id": 34,
+      "nome": "Abobrinha",
+      "categoria": "Vegetais",
+      "CaloriaPor100g": 17
+    },
+    {
+      "id": 35,
+      "nome": "Leite de Amêndoa",
+      "categoria": "Laticínios",
+      "CaloriaPor100g": 13
+    },
+    {
+      "id": 36,
+      "nome": "Kiwi",
+      "categoria": "Frutas",
+      "CaloriaPor100g": 61
+    },
+    {
+      "id": 37,
+      "nome": "Massa",
+      "categoria": "Grãos",
+      "CaloriaPor100g": 158
+    },
+    {
+      "id": 38,
+      "nome": "Linguado",
+      "categoria": "Peixes",
+      "CaloriaPor100g": 70
+    },
+    {
+      "id": 39,
+      "nome": "Espináfia",
+      "categoria": "Vegetais",
+      "CaloriaPor100g": 23
+    },
+    {
+      "id": 40,
+      "nome": "Leite de Coco",
+      "categoria": "Laticínios",
+      "CaloriaPor100g": 230
+    },
+    {
+      "id": 41,
+      "nome": "Pêssego",
+      "categoria": "Frutas",
+      "CaloriaPor100g": 39
+    },
+    {
+      "id": 42,
+      "nome": "Arroz Integral",
+      "categoria": "Grãos",
+      "CaloriaPor100g": 123
+    },
+    {
+      "id": 43,
+      "nome": "Tilápia",
+      "categoria": "Peixes",
+      "CaloriaPor100g": 96
+    },
+    {
+      "id": 44,
+      "nome": "Beterraba",
+      "categoria": "Vegetais",
+      "CaloriaPor100g": 43
+    },
+    {
+      "id": 45,
+      "nome": "Iogurte Grego",
+      "categoria": "Laticínios",
+      "CaloriaPor100g": 97
+    },
+    {
+      "id": 46,
+      "nome": "Cereja",
+      "categoria": "Frutas",
+      "CaloriaPor100g": 50
+    },
+    {
+      "id": 47,
+      "nome": "Cuscuz",
+      "categoria": "Grãos",
+      "CaloriaPor100g": 112
+    },
+    {
+      "id": 48,
+      "nome": "Salmão em Conserva",
+      "categoria": "Peixes",
+      "CaloriaPor100g": 180
+    },
+    {
+      "id": 49,
+      "nome": "Couve-Flor",
+      "categoria": "Vegetais",
+      "CaloriaPor100g": 25
+    },
+    {
+      "id": 50,
+      "nome": "Leite de Aveia",
+      "categoria": "Laticínios",
+      "CaloriaPor100g": 42
+    },
+    {
+      "id": 51,
+      "nome": "Abacate",
+      "categoria": "Frutas",
+      "CaloriaPor100g": 160
+    },
+    {
+      "id": 52,
+      "nome": "Feijão Preto",
+      "categoria": "Grãos",
+      "CaloriaPor100g": 339
+    },
+    {
+      "id": 53,
+      "nome": "Sardinha em Lata",
+      "categoria": "Peixes",
+      "CaloriaPor100g": 190
+    },
+    {
+      "id": 54,
+      "nome": "Aspargo",
+      "categoria": "Vegetais",
+      "CaloriaPor100g": 20
+    },
+    {
+      "id": 55,
+      "nome": "Queijo Feta",
+      "categoria": "Laticínios",
+      "CaloriaPor100g": 264
+    },
+    {
+      "id": 56,
+      "nome": "Pêssego em Calda",
+      "categoria": "Frutas",
+      "CaloriaPor100g": 43
+    },
+    {
+      "id": 57,
+      "nome": "Arroz Basmati",
+      "categoria": "Grãos",
+      "CaloriaPor100g": 121
+    },
+    {
+      "id": 58,
+      "nome": "Tilápia Grelhada",
+      "categoria": "Peixes",
+      "CaloriaPor100g": 128
+    },
+    {
+      "id": 59,
+      "nome": "Ervilhas Congeladas",
+      "categoria": "Vegetais",
+      "CaloriaPor100g": 81
+    },
+    {
+      "id": 60,
+      "nome": "Leite de Amêndoa",
+      "categoria": "Laticínios",
+      "CaloriaPor100g": 13
+    },
+    {
+      "id": 61,
+      "nome": "Tomate",
+      "categoria": "Vegetais",
+      "CaloriaPor100g": 18
+    },
+    {
+      "id": 62,
+      "nome": "Pêssego Fresco",
+      "categoria": "Frutas",
+      "CaloriaPor100g": 39
+    },
+    {
+      "id": 63,
+      "nome": "Aveia",
+      "categoria": "Grãos",
+      "CaloriaPor100g": 71
+    },
+    {
+      "id": 64,
+      "nome": "Salmão Defumado",
+      "categoria": "Peixes",
+      "CaloriaPor100g": 117
+    },
+    {
+      "id": 65,
+      "nome": "Pepino",
+      "categoria": "Vegetais",
+      "CaloriaPor100g": 16
+    },
+    {
+      "id": 66,
+      "nome": "Leite de Cabra",
+      "categoria": "Laticínios",
+      "CaloriaPor100g": 68
+    },
+    {
+      "id": 67,
+      "nome": "Manga",
+      "categoria": "Frutas",
+      "CaloriaPor100g": 60
+    },
+    {
+      "id": 68,
+      "nome": "Arroz Selvagem",
+      "categoria": "Grãos",
+      "CaloriaPor100g": 101
+    },
+    {
+      "id": 69,
+      "nome": "Salmão Selvagem",
+      "categoria": "Peixes",
+      "CaloriaPor100g": 367
+    },
+    {
+      "id": 70,
+      "nome": "Milho Doce",
+      "categoria": "Vegetais",
+      "CaloriaPor100g": 86
+    },
+    // procurar mais alimentos aqui
+  ]
+};
+
+function pesquisarAlimentos() {
+  const consulta = document.getElementById('alimento').value.toLowerCase();
+  const resultadoUl = document.getElementById('resultado');
+  resultadoUl.innerHTML = '';
+
+  if (!consulta) {
+    const errorElement = document.querySelector('.error');
+    errorElement.classList.remove('error-hidden');
+    return;
+  }
+
+  const alimentosFiltrados = al.alimentos.filter(alimento => alimento.nome.toLowerCase().includes(consulta));
+
+  if (alimentosFiltrados.length === 0) {
+    resultadoUl.innerHTML = 'Nenhum resultado encontrado.';
+  } else {
+    alimentosFiltrados.forEach(alimento => {
+      const li = document.createElement('li');
+      li.textContent = `Nome: ${alimento.nome}, Categoria: ${alimento.categoria}, Calorias: ${alimento.CaloriaPor100g}`;
+      resultadoUl.appendChild(li);
+    });
+  }
+}
+
+
+
